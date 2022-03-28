9a581b950cd2ae2d3493593a724753d318d14c14
ShSa <ShSa@C5107998>
2022-03-28 14:24:11 +0200

    Put your commit message!

---
+++ test3.tf
@@ -0,0 +1,23 @@
+module "sandbox" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+ AccountEmail              = "asf"
+ AccountName               = "wq"
+ ManagedOrganizationalUnit = "e"
+ SSOUserEmail              = "fas"
+ SSOUserFirstName          = "wqe"
+ SSOUserLastName           = "ads"
+}
+account_tags = {
+ "eqw"="edase"
+}
+change_management_parameters = {
+ change_requested_by = "qw"
+ change_reason       = "sad"
+}
+custom_fields = {
+ group = "wqe"
+}
+account_customizations_name = "asda"
+}
